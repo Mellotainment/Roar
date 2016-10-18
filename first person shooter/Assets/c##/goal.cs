9d53c0215aff5700e1053d4c63e432b85d2c7700
Mellotainment <winstoncc56@gmail.com>
2016-10-18 13:04:59 -0400

    Add files via upload

---
+++ first person shooter/Assets/c##/goal.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManager;
+
+public class goal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (other.gameObject.tag == "MainCamera")
+			Debug.log ("Oh NO");
+		SceneManager.LoadScene ("end");
+	}
+}
---
+++ first person shooter/Assets/c##/monster.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManager;
+
+public class monster : MonoBehaviour {
+
+	public float	enemySpeed = .5f;
+	public int 		dir = 1;
+	public float	offset = 4;
+
+
+	void Start () {
+
+	}
+
+
+	void Update () {
+
+		float t = Time.time - offset;
+
+
+
+		if(t > 5){
+			dir = -dir;
+			offset = Time.time;
+		
+
+		GetComponent<Rigidbody>().velocity = new Vector3(
+
+			enemySpeed * dir, 
+
+			GetComponent<Rigidbody>().velocity.x);
+	
+
+		void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "MainCamera")
+			Debug.log ("Oh NO");
+		SceneManager.LoadScene ("end");
+	}
+}
+	}
+}
+}
+			
